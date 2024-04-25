f3746b8b55f675ba9980538fe6e8c9cbe97f64ea
elyorbek <temirovelyor7347@gmail.com>
2024-04-25 12:00:56 +0500

    add files

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Footer from './loyout/Footer';
+import Header from './loyout/Header';
+import Main from './loyout/Main';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
